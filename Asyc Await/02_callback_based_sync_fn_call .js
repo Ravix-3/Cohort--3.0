925b73e7bd3bd856764e2cbe9e4e647824eafff6
Ravi  Kumar <132392267+Ravix-3@users.noreply.github.com>
2026-03-09 14:44:15 +0530

    Delete Asyc Await directory

--- Asyc Await/02_callback_based_sync_fn_call .js
+++
@@ -1,8 +0,0 @@
-// callback based , sync function calls...
-const fs = require('fs');
-
-let contents = fs.readFileSync("a.txt","utf-8");
-console.log(contents);
-let trimcontent = contents.trim();
-fs.writeFileSync("a.txt", trimcontent);
-console.log(trimcontent);
